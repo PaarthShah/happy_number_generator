f8f01015fe63427fa8951ed4507e66feedc9898e
Paarth Shah <mail@shahpaarth.com>
2022-01-29 14:03:32 -0800

    Full naive solution for Go.

--- Go 1.7/HappyNumberGenerator.go
+++ Go 1.7/HappyNumberGenerator.go
@@ -1,8 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+const LENLIST uint = 10_000_000
+
+// Happiness - true for happy, false for sad.
+type Happiness bool
 
 func sumOfSquares(num uint) uint {
 	var sum uint = 0
@@ -14,7 +17,7 @@ func sumOfSquares(num uint) uint {
 	return sum
 }
 
-func isHappy(num uint) bool {
+func isHappy(num uint) Happiness {
 	haveSeen := map[uint]bool{
 		num: true,
 	}
@@ -22,12 +25,22 @@ func isHappy(num uint) bool {
 		num = sumOfSquares(num)
 		if haveSeen[num] {
 			return false
+		} else {
+			haveSeen[num] = true
 		}
 	}
 	return true
 }
 
 func main() {
-	fmt.Println(isHappy(7))
-	fmt.Println(isHappy(4))
+	happyList := make([]uint, LENLIST)
+	var happyCounter uint = 0
+	var i uint = 1
+	for ; happyCounter < LENLIST; i++ {
+		if isHappy(i) {
+			happyList[happyCounter] = i
+			happyCounter += 1
+		}
+	}
+	fmt.Println(happyList[LENLIST-5:])
 }
